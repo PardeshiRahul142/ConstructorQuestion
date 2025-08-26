e620a0f2e8615287e7e9d427632db6f861ebb35b
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-08-26 12:06:22 +0530

    Add files via upload

---
+++ PrivateConstructor.java
@@ -0,0 +1,16 @@
+package gth.pck;
+class ayx
+{
+	 ayx()
+	{
+		System.out.println("I am  constructor");
+	}
+}
+public class PrivateConstructor {
+
+	public static void main(String[] args) {
+		ayx a=new ayx();
+
+	}
+
+}
